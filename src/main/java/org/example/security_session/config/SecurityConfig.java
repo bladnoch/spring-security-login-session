a4bf35c4990f977552339c63557ff63059219ef9
DK <dongwook5478@gmail.com>
2024-07-29 02:48:20 +0900

    feat: add admin.mustache, AdminController

--- src/main/java/org/example/security_session/config/SecurityConfig.java
+++ src/main/java/org/example/security_session/config/SecurityConfig.java
@@ -19,6 +19,10 @@ public class SecurityConfig {
          * 조건없이 main page로 접근이 가능하다
          */
         http
+                /**
+                 * 깔때기처럼 requestMathcers의 권한은 순서에 따라 주어지고 앞에서의 권한이 뒤어서도 적용되기 때문에
+                 * 넓은 범위의 권한부터 좁은 범위의 권한으로 설정해야 한다.
+                 */
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/login").permitAll() // 시작 페이지, 로그인 페이지 조건 없이 접근 허용
                         .requestMatchers("/admin").hasRole("ADMIN") // admin page는 role = "ADMIN" 만 접근 허용
