254d73c4384f27ccb49b50f59f98cec827f9a121
DK <dongwook5478@gmail.com>
2024-07-30 04:02:41 +0900

    feat: add BCryptPasswordEncoder for encode password

--- src/main/java/org/example/security_session/config/SecurityConfig.java
+++ src/main/java/org/example/security_session/config/SecurityConfig.java
@@ -1,15 +1,24 @@
 package org.example.security_session.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    /*
+    시큐리티 암호와 메소드: 암호화를 위한 메소드로 bean 등록을 해서 사용
+     */
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
