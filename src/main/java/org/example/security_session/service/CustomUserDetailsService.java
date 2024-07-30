7016225b6385e6d18215d66d01b5dd273a89dfa4
DK <dongwook5478@gmail.com>
2024-07-30 22:34:43 +0900

    feat: admin 접근을 위한 코드 작성중 리디렉션 문제 발생

--- src/main/java/org/example/security_session/service/CustomUserDetailsService.java
+++ src/main/java/org/example/security_session/service/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package org.example.security_session.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.security_session.domain.UserEntity;
+import org.example.security_session.dto.CustomUserDetails;
 import org.example.security_session.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,8 +19,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         UserEntity userData = userRepository.findByUsername(username);
+        System.out.println("CustomUserDetailsService.loadUserByUsername");
+        System.out.println("userData.getUsername = " + userData.getUsername());
+        System.out.println("userData.getRole() = " + userData.getRole());
 
         if (userData != null) {
+            System.out.println("userData exist");
             return new CustomUserDetails(userData);
         }
 
