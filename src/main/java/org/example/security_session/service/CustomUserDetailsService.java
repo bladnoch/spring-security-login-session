7e7ae0294d5eec44613cc56baa2a9fcddcb4e8d8
DK <dongwook5478@gmail.com>
2024-08-04 18:21:37 +0900

    feat: 계층 권한을 적용 거기에 따라 임의 의 role롤 수정했고 CustomUserDetailsService.java를 주석처리함

--- src/main/java/org/example/security_session/service/CustomUserDetailsService.java
+++ src/main/java/org/example/security_session/service/CustomUserDetailsService.java
@@ -1,33 +1,33 @@
-package org.example.security_session.service;
-
-import lombok.RequiredArgsConstructor;
-import org.example.security_session.domain.UserEntity;
-import org.example.security_session.dto.CustomUserDetails;
-import org.example.security_session.repository.UserRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
-
-    private final UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        UserEntity userData = userRepository.findByUsername(username);
-        System.out.println("CustomUserDetailsService.loadUserByUsername");
-        System.out.println("userData.getUsername = " + userData.getUsername());
-        System.out.println("userData.getRole() = " + userData.getRole());
-
-        if (userData != null) {
-            System.out.println("userData exist");
-            return new CustomUserDetails(userData);
-        }
-
-        return null;
-    }
-}
+//package org.example.security_session.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.example.security_session.domain.UserEntity;
+//import org.example.security_session.dto.CustomUserDetails;
+//import org.example.security_session.repository.UserRepository;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    private final UserRepository userRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//
+//        UserEntity userData = userRepository.findByUsername(username);
+//        System.out.println("CustomUserDetailsService.loadUserByUsername");
+//        System.out.println("userData.getUsername = " + userData.getUsername());
+//        System.out.println("userData.getRole() = " + userData.getRole());
+//
+//        if (userData != null) {
+//            System.out.println("userData exist");
+//            return new CustomUserDetails(userData);
+//        }
+//
+//        return null;
+//    }
+//}
